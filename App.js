b94a3fcf89455b663ebef09840fda8217051c6a5
Sascha Rissling <sascha.rissling@gmail.com>
2020-01-21 16:14:46 +0100

    finish first basic app

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Button } from "react-native";
+import styled from "@emotion/native";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background: black;
+`;
+
+const AppText = styled.Text`
+  color: #707070;
+  font-size: 30px;
+`;
 
 export default function App() {
+  const [text, setText] = React.useState("First React App");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Container>
+      <AppText>{text}</AppText>
+      <Button
+        title="Change Text"
+        onPress={() => setText("the text has been changed!")}
+      />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
